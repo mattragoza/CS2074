10ae011a2626c62d05f7cc3c6e14d3f45aa60db5
Matt Ragoza <mtr22@pitt.edu>
2023-09-28 18:19:45 -0400

    add clusters and hw 4 part 2 wip

--- hw4/computeBOWrepr.m
+++ hw4/computeBOWrepr.m
@@ -1,10 +1,19 @@
 function [bow_repr] = computeBOWrepr(features, means)
 
+num_points = size(features, 1);
 num_clusters = size(means, 1);
+num_features = size(means, 2);
 
-bow_repr = zeros(num_clusters);
+% compute distances b/tw keypoints and cluster means
+dist2 = pdist2(features, means);
 
-% normalize the BOW representation
+% assign clusters to each keypoint based on minimum distance
+min_inds = (dist2 == min(dist2, [], 2));
+
+% count the points that were assigned to each cluster
+bow_repr = sum(min_inds, 1);
+
+% normalize the bag-of-words representation
 bow_repr = bow_repr ./ sum(bow_repr);
 
 end
--- hw4/hw4.m
+++ hw4/hw4.m
@@ -1,12 +1,12 @@
 clc; clear; debug = 1;
 
 image = imread('images/ella.jpg');
+load('clusters/means_k2.mat', 'means');
 
 %% Part 1
 [x, y, scores, Ix, Iy] = extract_keypoints(image, debug);
 display_keypoints(image, x, y, scores, Ix, Iy);
-features = compute_features(x, y, scores, Ix, Iy)
+features = compute_features(x, y, scores, Ix, Iy);
 
 %% Part 2
-%means = ...
-%bow_repr = computeBOWrepr(features, means);
+bow_repr = computeBOWrepr(features, means);
